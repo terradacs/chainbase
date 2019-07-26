bf5c41aa8bdbd505844858cf396745680aa8dc67
johndebord <john.debord@block.one>
2019-07-26 16:42:09 -0400

    Continuation

--- test/token_transfer_emulation.cpp
+++ test/token_transfer_emulation.cpp
@@ -12,77 +12,62 @@
 #include <boost/test/unit_test.hpp>
 #include <chainbase/chainrocks.hpp>
 
-#define MEASURE_START(current_test)                                                              \
+#define MEASURE_START                                                                            \
    {                                                                                             \
-   outfile << "-------STARTING MEASUREMENT-------" << '\n';                                      \
-   outfile << "Currently testing: " current_test   << '\n';                                      \
-   std::chrono::high_resolution_clock::time_point t1{std::chrono::high_resolution_clock::now()}; \
+   std::cout << "-------STARTING MEASUREMENT-------" << '\n';                                    \
+   std::chrono::high_resolution_clock::time_point t1{std::chrono::high_resolution_clock::now()};
 
 #define INTERMEDIATE_MEASUREMENT                                                                             \
-   outfile << "*******STARTING INTERMEDIATE MEASUREMENT*******" << '\n';                                     \
    std::chrono::high_resolution_clock::time_point tmp_time_point{std::chrono::high_resolution_clock::now()}; \
    auto tmp_duration{std::chrono::duration_cast<std::chrono::microseconds>(tmp_time_point-t1).count()};      \
-   outfile << "Microseconds: " << tmp_duration << '\n';                                                      \
-   outfile << "*******STOPPING INTERMEDIATE MEASUREMENT*******" << "\n\n";                                   \
+   _milliseconds << tmp_duration << ',';
 
 #define MEASURE_STOP                                                                             \
    std::chrono::high_resolution_clock::time_point t2{std::chrono::high_resolution_clock::now()}; \
    auto duration{std::chrono::duration_cast<std::chrono::microseconds>(t2-t1).count()};          \
-   outfile << "Microseconds: " << duration     << '\n';                                          \
-   outfile << "-------STOPPING MEASUREMENT-------" << "\n\n";                                    \
-   }                                                                                             \
+   std::cout << "-------STOPPING MEASUREMENT-------" << '\n';                                    \
+   _total_time << duration;                                                                      \
+   }
 
 class generated_data {
 public:
-   generated_data(size_t num_of_keys_and_values,
+   generated_data(size_t num_of_accounts_and_values,
                   size_t num_of_swaps,
                   size_t lower_bound_inclusive,
                   size_t upper_bound_inclusive)
       : _dre{static_cast<unsigned int>(time(0))}
       , _uid{lower_bound_inclusive, upper_bound_inclusive}
-      , _num_of_keys_and_values{num_of_keys_and_values}
+      , _num_of_accounts_and_values{num_of_accounts_and_values}
       , _num_of_swaps{num_of_swaps}
-      
+
    {
       _generate_values();
    }
 
-   void print_accounts() {
-      std::cout << "_accounts:\n";
-      for (auto e : _accounts) {
-         std::cout << e << '\n';
-      }
+   inline void print_accounts() {
+      _print_something("_accounts", _accounts);
    }
 
-   void print_values() {
-      std::cout << "_values:\n";
-      for (auto e : _values) {
-         std::cout << e << '\n';
-      }
+   inline void print_values() {
+      _print_something("_values", _values);
    }
 
-   void print_swaps0() {
-      std::cout << "_swaps0:\n";
-      for (auto e : _swaps0) {
-         std::cout << e << '\n';
-      }
+   inline void print_swaps0() {
+      _print_something("_swaps0", _swaps0);
    }
 
-   void print_swaps1() {
-      std::cout << "_swaps1:\n";
-      for (auto e : _swaps1) {
-         std::cout << e << '\n';
-      }
+   inline void print_swaps1() {
+      _print_something("_swaps1", _swaps1);
    }
 
-   inline const size_t num_of_keys_and_values() const {
-      return _num_of_keys_and_values;
+   inline const size_t num_of_accounts_and_values() const {
+      return _num_of_accounts_and_values;
    }
 
    inline const size_t num_of_swaps() const {
       return _num_of_swaps;
    }
-   
+
    inline const std::vector<size_t>& accounts() const {
       return _accounts;
    }
@@ -102,20 +87,20 @@ public:
 private:
    std::default_random_engine _dre;
    std::uniform_int_distribution<size_t> _uid;
-   
-   size_t _num_of_keys_and_values;
+
+   size_t _num_of_accounts_and_values;
    size_t _num_of_swaps;
-   
+
    std::vector<size_t> _accounts;
    std::vector<size_t> _values;
    std::vector<size_t> _swaps0;
    std::vector<size_t> _swaps1;
 
    inline void _generate_values() {
-      std::cout << "Generating values...\n";
-      
+      std::cout << "Generating values... ";
+
       size_t count{0};
-      while (count < _num_of_keys_and_values) {
+      while (count < _num_of_accounts_and_values) {
          _accounts.push_back(_generate_value());
          _values.push_back(_generate_value());
          ++count;
@@ -123,80 +108,109 @@ private:
 
       count = 0;
       while (count < _num_of_swaps) {
-         _swaps0.push_back(_generate_value()%_num_of_keys_and_values);
-         _swaps1.push_back(_generate_value()%_num_of_keys_and_values);
+         _swaps0.push_back(_generate_value()%_num_of_accounts_and_values);
+         _swaps1.push_back(_generate_value()%_num_of_accounts_and_values);
          ++count;
       }
-      
-      std::cout << "Done.\n";
+
+      std::cout << "done.\n";
    }
 
    inline size_t _generate_value() {
       return _uid(_dre);
    }
+
+   inline void _print_something(const std::string& vec_name, const std::vector<size_t>& vec) {
+      std::cout << vec_name << ": \n";
+      size_t count{};
+      for (auto e : vec) {
+         std::cout << e << '\t';
+         ++count;
+         if ((count % 10) == 0) {
+            std::cout << '\n';
+         }
+      }
+   }
 };
 
 class database_test {
 public:
-   database_test(size_t num_of_keys_and_values,
+   database_test(size_t num_of_accounts_and_values,
                  size_t num_of_swaps,
                  size_t lower_bound_inclusive,
                  size_t upper_bound_inclusive)
-      : _output  {"/Users/john.debord/chainbase/build/test/outfile"}
-      , _database{"/Users/john.debord/chainbase/build/test/data"}
-      , _gen_data{num_of_keys_and_values, num_of_swaps, lower_bound_inclusive, upper_bound_inclusive}
-      
+      : _milliseconds{"/Users/john.debord/chainbase/build/test/milliseconds"}
+      , _ram_usage   {"/Users/john.debord/chainbase/build/test/ram_usage"}
+      , _total_time  {"/Users/john.debord/chainbase/build/test/total_time"}
+      , _database    {"/Users/john.debord/chainbase/build/test/data"}
+      , _gen_data    {num_of_accounts_and_values, num_of_swaps, lower_bound_inclusive, upper_bound_inclusive}
    {
+      _log_data.reserve((_gen_data.num_of_swaps()*0.01)*2);
    }
 
    ~database_test() {
       boost::filesystem::remove_all("/Users/john.debord/chainbase/build/test/data");
    }
 
+   inline void print_everything() {
+      _gen_data.print_accounts();
+      _gen_data.print_values();
+      _gen_data.print_swaps0();
+      _gen_data.print_swaps1();
+   }
+
    inline void start_test() {
       _initial_database_state();
       _execution_loop();
-      // Log statistics.
    }
-   
-private:
-   std::ofstream        _output;
-   chainrocks::database _database;
-   generated_data       _gen_data;
 
-   // Simulating 300 blocks of account/value initializations.
-   // Therefore with 1,000,000 unique accounts and values, there shall
-   // be 3,333 new accounts/values per block (AKA
-   // `start_undo_session(true)`).  After which, the undo session
-   // shall be pushed onto the undo stack.
+private:
+   std::ofstream                          _milliseconds;
+   std::ofstream                          _ram_usage;
+   std::ofstream                          _total_time;
+   chainrocks::database                   _database;
+   generated_data                         _gen_data;
+   std::vector<std::chrono::microseconds> _log_data{};
+
    inline void _initial_database_state() {
-      for (size_t i{}; i < _gen_data.num_of_keys_and_values(); ++i) {
+      std::cout << "Filling initial database state... ";
+      for (size_t i{}; i < _gen_data.num_of_accounts_and_values(); ++i) {
          auto session{_database.start_undo_session(true)};
          _database.put(_gen_data.accounts()[i], std::to_string(_gen_data.values()[i]));
          session.push();
       }
+      _database.start_undo_session(true).push();
+      std::cout << "done.\n";
    }
 
    inline void _execution_loop() {
-      _database.print_state();
-      _gen_data.print_swaps0();
-      _gen_data.print_swaps1();
+      MEASURE_START;
+      std::cout << "Benchmarking... ";
       for (size_t i{}; i < _gen_data.num_of_swaps(); ++i) {
+         if (__builtin_expect(((size_t)i % (size_t)(_gen_data.num_of_swaps() * 0.01)) == 0, false)) {
+            INTERMEDIATE_MEASUREMENT;
+         }
+
          auto rand_account0{_gen_data.accounts()[_gen_data.swaps0()[i]]};
          auto rand_account1{_gen_data.accounts()[_gen_data.swaps1()[i]]};
-         
+
+         auto session{_database.start_undo_session(true)};
          auto tmp{_database.get(rand_account0).second};
          _database.put(rand_account0, _database.get(rand_account1).second);
          _database.put(rand_account1, tmp);
+         session.squash();
       }
-      _database.print_state();
+         std::cout << "done.\n";
+         MEASURE_STOP;
    }
 };
 
 BOOST_AUTO_TEST_CASE(test_one) {
-   database_test dt{10,10,0,10};
+   const static size_t num_of_accounts_and_values{1000000};
+   const static size_t num_of_swaps{1000000};
+   const static size_t lower_bound_inclusive{0};
+   const static size_t upper_bound_inclusive{std::numeric_limits<size_t>::max()};
+
+   database_test dt{num_of_accounts_and_values, num_of_swaps, lower_bound_inclusive, upper_bound_inclusive};
    dt.start_test();
-   // generated_data gd{1000000, 0, std::numeric_limits<size_t>::max()};
-   // gd.print_accounts();
-   // gd.print_values();
 BOOST_AUTO_TEST_SUITE_END()
