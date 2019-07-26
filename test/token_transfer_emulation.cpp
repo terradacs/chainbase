1b6ecc8b594e7aa7cafdbee97d41daa757bc472d
johndebord <john.debord@block.one>
2019-07-26 10:39:23 -0400

    Test logic improved

--- test/token_transfer_emulation.cpp
+++ test/token_transfer_emulation.cpp
@@ -12,8 +12,6 @@
 #include <boost/test/unit_test.hpp>
 #include <chainbase/chainrocks.hpp>
 
-#define LOG
-
 #define MEASURE_START(current_test)                                                              \
    {                                                                                             \
    outfile << "-------STARTING MEASUREMENT-------" << '\n';                                      \
@@ -34,102 +32,171 @@
    outfile << "-------STOPPING MEASUREMENT-------" << "\n\n";                                    \
    }                                                                                             \
 
-/// Note: use `exponential_distribution` in the future to simulate more
-/// accounts with values at the lower bound as opposed to the upper bound.
-static const uint64_t lower_bound_inclusive{0};
-static const uint64_t upper_bound_inclusive{9};
-static std::uniform_int_distribution<uint64_t> uid{lower_bound_inclusive, upper_bound_inclusive};
-static std::default_random_engine dre(time(0));
-
-class generated_datum {
+class generated_data {
 public:
-   generated_datum(size_t size)
-      : _size{size}
+   generated_data(size_t num_of_keys_and_values,
+                  size_t num_of_swaps,
+                  size_t lower_bound_inclusive,
+                  size_t upper_bound_inclusive)
+      : _dre{static_cast<unsigned int>(time(0))}
+      , _uid{lower_bound_inclusive, upper_bound_inclusive}
+      , _num_of_keys_and_values{num_of_keys_and_values}
+      , _num_of_swaps{num_of_swaps}
+      
    {
-      _generate_values(size);
+      _generate_values();
    }
 
-   const uint64_t& operator[](size_t n) const {
-      return _values[n];
+   void print_accounts() {
+      std::cout << "_accounts:\n";
+      for (auto e : _accounts) {
+         std::cout << e << '\n';
+      }
    }
 
-   void regenerate_values(size_t size) {
-      _values.clear();
-      _generate_values(size);
+   void print_values() {
+      std::cout << "_values:\n";
+      for (auto e : _values) {
+         std::cout << e << '\n';
+      }
+   }
+
+   void print_swaps0() {
+      std::cout << "_swaps0:\n";
+      for (auto e : _swaps0) {
+         std::cout << e << '\n';
+      }
+   }
+
+   void print_swaps1() {
+      std::cout << "_swaps1:\n";
+      for (auto e : _swaps1) {
+         std::cout << e << '\n';
+      }
+   }
+
+   inline const size_t num_of_keys_and_values() const {
+      return _num_of_keys_and_values;
+   }
+
+   inline const size_t num_of_swaps() const {
+      return _num_of_swaps;
    }
    
-   const std::vector<uint64_t>& data() const {
+   inline const std::vector<size_t>& accounts() const {
+      return _accounts;
+   }
+
+   inline const std::vector<size_t>& values() const {
       return _values;
    }
 
-   const size_t size() const {
-      return _size;
+   inline const std::vector<size_t>& swaps0() const {
+      return _swaps0;
+   }
+
+   inline const std::vector<size_t>& swaps1() const {
+      return _swaps1;
    }
 
 private:
-   std::vector<uint64_t> _values;
-   size_t _size;
+   std::default_random_engine _dre;
+   std::uniform_int_distribution<size_t> _uid;
+   
+   size_t _num_of_keys_and_values;
+   size_t _num_of_swaps;
+   
+   std::vector<size_t> _accounts;
+   std::vector<size_t> _values;
+   std::vector<size_t> _swaps0;
+   std::vector<size_t> _swaps1;
 
-   /// Note: To generate random byte arrays, construct a char table.
-   /// Then index randomly into the char array an append it to the given array.
-   inline void _generate_values(size_t size) {
+   inline void _generate_values() {
       std::cout << "Generating values...\n";
+      
       size_t count{0};
-      while (count < size) {
-         uint64_t value{uid(dre)};
-         auto iter{std::find(_values.cbegin(), _values.cend(), value)};
-         if (iter == _values.cend()) {
-            _values.push_back(value);
-            ++count;
-         }
-         else {
-            continue;
-         }
+      while (count < _num_of_keys_and_values) {
+         _accounts.push_back(_generate_value());
+         _values.push_back(_generate_value());
+         ++count;
       }
+
+      count = 0;
+      while (count < _num_of_swaps) {
+         _swaps0.push_back(_generate_value()%_num_of_keys_and_values);
+         _swaps1.push_back(_generate_value()%_num_of_keys_and_values);
+         ++count;
+      }
+      
       std::cout << "Done.\n";
+   }
+
+   inline size_t _generate_value() {
+      return _uid(_dre);
    }
 };
 
-std::ofstream outfile{"/Users/john.debord/chainbase/build/test/outfile"};
-chainrocks::database database{"/Users/john.debord/chainbase/build/test/data"};
-std::set<uint64_t> keys{};
-std::set<uint64_t> values{};
-generated_datum random_keys{10};
-generated_datum random_values{10};
-generated_datum random_indices{10};
-
-void initial_database_state() {
-   // Simulating 300 blocks of account/value initializations.
-   // Therefore with 1,000,000 unique accounts and values, there shall be
-   // 3,333 new accounts/values per block (AKA `start_undo_session(true)`).
-   // After which, the undo session shall be pushed onto the undo stack.
-   for (size_t i{}; i < random_keys.size(); ++i) {
-      auto session{database.start_undo_session(true)};
-      database.put(random_keys[i], std::to_string(random_values[i]));
-      keys.insert(random_keys[i]);
-      values.insert(random_values[i]);
-      session.push();
-   }
-}
-
-void execution_loop() {
-   database.print_state();
-   
-   for (auto e : random_indices.data()) {
-      std::cout << "e:                           " << e                             << "\n";
-      std::cout << "e+1%10:                      " << ((e+1)%10)                    << "\n";
-      auto tmp{database.get(e).second};
-      std::cout << "tmp:                         " << tmp                           << "\n";
-      std::cout << "database.get(e+1%10).second: " << database.get((e+1)%10).second << "\n\n";
+class database_test {
+public:
+   database_test(size_t num_of_keys_and_values,
+                 size_t num_of_swaps,
+                 size_t lower_bound_inclusive,
+                 size_t upper_bound_inclusive)
+      : _output  {"/Users/john.debord/chainbase/build/test/outfile"}
+      , _database{"/Users/john.debord/chainbase/build/test/data"}
+      , _gen_data{num_of_keys_and_values, num_of_swaps, lower_bound_inclusive, upper_bound_inclusive}
       
-      database.put(e, database.get((e+1)%10).second);
-      database.put((e+1)%10, tmp);
+   {
    }
-   database.print_state();
-}
+
+   ~database_test() {
+      boost::filesystem::remove_all("/Users/john.debord/chainbase/build/test/data");
+   }
+
+   inline void start_test() {
+      _initial_database_state();
+      _execution_loop();
+      // Log statistics.
+   }
+   
+private:
+   std::ofstream        _output;
+   chainrocks::database _database;
+   generated_data       _gen_data;
+
+   // Simulating 300 blocks of account/value initializations.
+   // Therefore with 1,000,000 unique accounts and values, there shall
+   // be 3,333 new accounts/values per block (AKA
+   // `start_undo_session(true)`).  After which, the undo session
+   // shall be pushed onto the undo stack.
+   inline void _initial_database_state() {
+      for (size_t i{}; i < _gen_data.num_of_keys_and_values(); ++i) {
+         auto session{_database.start_undo_session(true)};
+         _database.put(_gen_data.accounts()[i], std::to_string(_gen_data.values()[i]));
+         session.push();
+      }
+   }
+
+   inline void _execution_loop() {
+      _database.print_state();
+      _gen_data.print_swaps0();
+      _gen_data.print_swaps1();
+      for (size_t i{}; i < _gen_data.num_of_swaps(); ++i) {
+         auto rand_account0{_gen_data.accounts()[_gen_data.swaps0()[i]]};
+         auto rand_account1{_gen_data.accounts()[_gen_data.swaps1()[i]]};
+         
+         auto tmp{_database.get(rand_account0).second};
+         _database.put(rand_account0, _database.get(rand_account1).second);
+         _database.put(rand_account1, tmp);
+      }
+      _database.print_state();
+   }
+};
 
 BOOST_AUTO_TEST_CASE(test_one) {
-   initial_database_state();
-   execution_loop();
-   boost::filesystem::remove_all("/Users/john.debord/chainbase/build/test/data");
+   database_test dt{10,10,0,10};
+   dt.start_test();
+   // generated_data gd{1000000, 0, std::numeric_limits<size_t>::max()};
+   // gd.print_accounts();
+   // gd.print_values();
 BOOST_AUTO_TEST_SUITE_END()
